d3baf6e58bbea41f9bdbe6ab314e713eb563abdd
Evosoul <Wingrich@Web.de>
2021-05-22 12:01:01 +0200

    Added Organizer and some .css files

--- server/view/organizer_v.js
+++ server/view/organizer_v.js
@@ -9,19 +9,19 @@ const PATH = require('path');
 const PUG = require('pug');
 
 class LocalEventView_cl {
-   constructor () {
-      let baseDir_a = PATH.dirname(require.main.filename).split(PATH.sep);
-      baseDir_a.pop();
-      let baseDir_s = baseDir_a.join(PATH.sep);
-      let path_s = PATH.resolve(baseDir_s, 'client', 'templates', 'localeventlist.pug');
-      this.compiledListTemplate_p = PUG.compileFile(path_s);
+    constructor() {
+        let baseDir_a = PATH.dirname(require.main.filename).split(PATH.sep);
+        baseDir_a.pop();
+        let baseDir_s = baseDir_a.join(PATH.sep);
+        let path_s = PATH.resolve(baseDir_s, 'client', 'templates', 'organizerhome.pug');
+        this.compiledListTemplate_p = PUG.compileFile(path_s);
 
-   }
+    }
 
-   list_px (data_apl) {
-      let markup_s = this.compiledListTemplate_p({allLocalEvents_apl: data_apl});
-      return markup_s;
-   }
+    list_px(data_apl) {
+        let markup_s = this.compiledListTemplate_p({ allLocalEvents_apl: data_apl });
+        return markup_s;
+    }
 }
 module.exports = new LocalEventView_cl();
 // EOF
