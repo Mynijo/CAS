031b3d6ef0675d3898d920914aae26aed8309c75
Evosoul <Wingrich@Web.de>
2021-06-03 15:22:45 +0200

    weiter

--- server/controller/user_c.js
+++ server/controller/user_c.js
@@ -14,12 +14,12 @@ const UserHomeView_o = require('../view/user_v');
 
 let currentUserId_s = "";
 
-router_o.get(ressourcePath_s, function (req_opl, res_ops) {
+router_o.get(ressourcePath_s, function(req_opl, res_ops) {
     // nur zum Ausprobieren der verschiedenen Möglichkeiten, die Pfade beim Routing anzugeben
     res_ops.redirect("/");
 });
 
-router_o.post(ressourcePath_s, function (req_opl, res_ops) {
+router_o.post(ressourcePath_s, function(req_opl, res_ops) {
     let allUser_o = userData_o.get_all();
 
     let found_b = false;
@@ -48,50 +48,50 @@ router_o.post(ressourcePath_s, function (req_opl, res_ops) {
     this.currentUserId_s = activeUser.id_s;
 });
 
-router_o.get(ressourcePath_s + "/home/:id", function (req_opl, res_ops) {
+router_o.get(ressourcePath_s + "/home/:id", function(req_opl, res_ops) {
     let data_a = userData_o.get_by_id(req_opl.params.id);
     let dataEvent_a = localEventData_o.get_all();
     let markup_s = UserHomeView_o.home_px(data_a, dataEvent_a);
     res_ops.send(markup_s);
 });
 
-router_o.get(ressourcePath_s + "/localeventuserinformation/:id", function (req_opl, res_ops) {
+router_o.get(ressourcePath_s + "/localeventuserinformation/:id", function(req_opl, res_ops) {
     let data_a = userData_o.get_by_id(this.currentUserId_s);
     let dataEvent_a = localEventData_o.get_by_id(req_opl.params.id);
     let markup_s = UserHomeView_o.localeventuserinformation_px(data_a, dataEvent_a);
     res_ops.send(markup_s);
 });
 
-router_o.post(ressourcePath_s + '/add_sugg/:id', function (req_opl, res_ops) {
+router_o.post(ressourcePath_s + '/add_sugg/:id', function(req_opl, res_ops) {
     let id_s = userData_o.add_sugg(req_opl.body, req_opl.params.id);
     res_ops.redirect('/organizer/UserList');
 });
 
-router_o.post(ressourcePath_s + '/del_sugg/:id', function (req_opl, res_ops) {
+router_o.post(ressourcePath_s + '/del_sugg/:id', function(req_opl, res_ops) {
     let id_s = userData_o.del_sugg(req_opl.body, req_opl.params.id);
     res_ops.redirect('/organizer/UserList');
 });
 
-router_o.post(ressourcePath_s + '/add_book/:id', function (req_opl, res_ops) {
+router_o.post(ressourcePath_s + '/add_book/:id', function(req_opl, res_ops) {
     let id_s = userData_o.add_book(req_opl.body, req_opl.params.id);
     res_ops.redirect('/organizer/UserList');
 });
 
-router_o.post(ressourcePath_s + '/del_book/:id', function (req_opl, res_ops) {
+router_o.post(ressourcePath_s + '/del_book/:id', function(req_opl, res_ops) {
     let id_s = userData_o.del_book(req_opl.body, req_opl.params.id);
 });
 
-router_o.post(ressourcePath_s + '/create/:id', function (req_opl, res_ops) {
+router_o.post(ressourcePath_s + '/create/:id', function(req_opl, res_ops) {
     let id_s = userData_o.add(req_opl.body);
     res_ops.redirect('/organizer/UserList');
 });
 
-router_o.post(ressourcePath_s + '/save/:id', function (req_opl, res_ops) {
+router_o.post(ressourcePath_s + '/save/:id', function(req_opl, res_ops) {
     userData_o.set_by_id(req_opl.body, req_opl.params.id);
     res_ops.redirect('/organizer/UserList');
 });
 
-router_o.post(ressourcePath_s + '/delete/:id', function (req_opl, res_ops) {
+router_o.post(ressourcePath_s + '/delete/:id', function(req_opl, res_ops) {
     userData_o.del(req_opl.params.id);
     res_ops.redirect('/organizer/UserList');
 });
